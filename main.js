78d7fb3d910dcbb67c2834feda52e2fac02b29f8
Fred Agyeman <fredyliby@verizon.net>
2015-12-19 21:48:13 -0500

    modified home page

---
+++ main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $(".image2").click(function(){
+        $(".image2").hide();
+    });
+    $(".image2").click(function(){
+        $(".image2").show();
+    });
+});
